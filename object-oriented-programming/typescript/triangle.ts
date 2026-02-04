2a817d8bf295b34b6a527d0a0e189c0740e7b9a3
Manuel <manu.cruz56@hotmail.com>
2026-02-04 16:59:48 -0600

    feat(typescript): migrate objects files to ts extension

---
+++ object-oriented-programming/typescript/triangle.ts
@@ -0,0 +1,47 @@
+/*
+Un triangulo, una forma geometrica fundamental, es un poligono caracterizado por tener tres lados y
+tres vertices. De acuerdo con el teorema de la desigualdad del triangulo, en cualquier triangulo 
+valido, la suma de las longitudes de dos lados cualesquiera debe ser mayor que la longitud del tercer
+lado.
+
+Tu tarea es crear un objeto que represente un triangulo.
+
+Este objeto debe tener tres propiedades, cada una correspondiente a uno de sus lados.
+
+Ademas, se requiere una funcion para validar si los lados dados cumplen con el teorema de la 
+desigualdad del triangulo.
+
+Por ejemplo, considera un triangulo con longitudes de lados: a = 7, b = 10, y c = 5.
+
+Para determinar si estos lados forman un triangulo valido, evaluamos si la suma de cada par de lados
+es mayor que la longitud del lado restante. 
+
+En este caso, verificamos si (b + c) > a, (a + c) > b y (a + b) > c.
+
+Si se cumplen las tres condiciones, entonces los lados efectivamente forman un triangulo.
+
+*/
+interface Triangle {
+    a: number
+    b: number
+    c: number
+}
+const t1: Triangle = {
+    a: 7,
+    b: 10,
+    c: 5
+}
+
+const t2: Triangle = {
+    a: 2,
+    b: 3,
+    c: 6
+}
+
+const isValidTriangle = (triangle: number) =>{
+    const {a, b, c} = triangle
+     return (b + c) > a && (a + c) > b && (a + b) > c
+}
+
+console.log(isValidTriangle(t1))
+console.log(isValidTriangle(t2))
